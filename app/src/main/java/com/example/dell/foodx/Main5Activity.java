5877143318b52ffa0890faf42415b9ac602419c3
= <sahil.070197@gmail.com>
2018-10-18 15:41:34 +0530

    My changes

--- app/src/main/java/com/example/dell/foodx/Main5Activity.java
+++ app/src/main/java/com/example/dell/foodx/Main5Activity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class Main5Activity extends AppCompatActivity {
 
+	//changed code
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
