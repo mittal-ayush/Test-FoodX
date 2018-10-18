26615684152fbbb7c86519d8058bb308a9d874b2
Ayush Mittal <ayushmittal0297@gmail.com>
2018-10-18 15:43:51 +0530

    Ayush's changes

--- app/src/main/java/com/example/dell/foodx/Main5Activity.java
+++ app/src/main/java/com/example/dell/foodx/Main5Activity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main5Activity extends AppCompatActivity {
-
+//Hi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
